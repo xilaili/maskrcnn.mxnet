282ea9a2cd4fbc7cf42c7bfe4c81b92a01447a8b
xli47 <xli47@ncsu.edu>
2017-06-12 23:14:27 -0400

    improved demo

--- maskrcnn_demo.py
+++ maskrcnn_demo.py
@@ -9,9 +9,9 @@ from rcnn.symbol import get_resnet_test
 from rcnn.io.image import resize, transform
 import numpy as np
 # get config
-#os.environ['PYTHONUNBUFFERED'] = '1'
-#os.environ['MXNET_CUDNN_AUTOTUNE_DEFAULT'] = '0'
-#os.environ['MXNET_ENABLE_GPU_P2P'] = '0'
+os.environ['PYTHONUNBUFFERED'] = '1'
+os.environ['MXNET_CUDNN_AUTOTUNE_DEFAULT'] = '0'
+os.environ['MXNET_ENABLE_GPU_P2P'] = '0'
 
 import mxnet as mx
 from rcnn.core.tester import im_detect, Predictor
@@ -23,15 +23,7 @@ from rcnn.processing.nms import py_nms_wrapper
 from rcnn.mask.mask_transform import gpu_mask_voting, cpu_mask_voting
 from rcnn.processing.bbox_transform import clip_boxes
 
-
-def main():
-    # get symbol
-    ctx = mx.gpu(0)
-    sym = get_resnet_test(num_classes=config.NUM_CLASSES, num_anchors=config.NUM_ANCHORS)
-
-    # set up class names
-    num_classes = 81
-    classes = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat',
+CLASSES = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat',
                'traffic light', 'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse',
                'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie',
                'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove',
@@ -40,7 +32,11 @@ def main():
                'cake', 'chair', 'couch', 'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse',
                'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book',
                'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush']
+config.NUM_CLASSES = 81
+data_names = ['data', 'im_info']
+label_names = []
 
+def load_data():
     # load demo data
     image_names = ['COCO_test2015_000000000275.jpg', 'COCO_test2015_000000001412.jpg', 'COCO_test2015_000000073428.jpg',
                     'COCO_test2015_000000393281.jpg']
@@ -55,20 +51,24 @@ def main():
         im_info = np.array([[im_tensor.shape[2], im_tensor.shape[3], im_scale]], dtype=np.float32)
         data.append({'data': im_tensor, 'im_info': im_info})
 
+    return data, image_names
+
+def get_net(data, sym, prefix, epoch, ctx):
     # get predictor
-    data_names = ['data', 'im_info']
-    label_names = []
     data = [[mx.nd.array(data[i][name]) for name in data_names] for i in xrange(len(data))]
     max_data_shape = [('data', (1, 3, max([v[0] for v in config.SCALES]), max([v[1] for v in config.SCALES])))]
     provide_data = [[(k, v.shape) for k, v in zip(data_names, data[i])] for i in xrange(len(data))]
     provide_label = [None for i in xrange(len(data))]
-    arg_params, aux_params = load_param('model/e2e', 1, process=True)
+    arg_params, aux_params = load_param(prefix, epoch, process=True)
     print data_names, label_names, ctx, max_data_shape, provide_data, provide_label
     predictor = Predictor(sym, data_names, label_names,
                           context=[ctx], max_data_shapes=max_data_shape,
                           provide_data=provide_data, provide_label=provide_label,
                           arg_params=arg_params, aux_params=aux_params)
+    return predictor
 
+def demo_net(predictor, data, image_names):
+    data = [[mx.nd.array(data[i][name]) for name in data_names] for i in xrange(len(data))]
     # warm up
     for i in xrange(2):
         data_batch = mx.io.DataBatch(data=[data[0]], label=[], pad=0, index=0,
@@ -89,10 +89,10 @@ def main():
         im_shapes = [data_batch.data[i][0].shape[2:4] for i in xrange(len(data_batch.data))]
 
         if not config.TEST.USE_MASK_MERGE:
-            all_boxes = [[] for _ in xrange(num_classes)]
-            all_masks = [[] for _ in xrange(num_classes)]
+            all_boxes = [[] for _ in xrange(config.NUM_CLASSES)]
+            all_masks = [[] for _ in xrange(config.NUM_CLASSES)]
             nms = py_nms_wrapper(config.TEST.NMS)
-            for j in range(1, num_classes):
+            for j in range(1, config.NUM_CLASSES):
                 indexes = np.where(scores[0][:, j] > 0.7)[0]
                 cls_scores = scores[0][indexes, j, np.newaxis]
                 cls_masks = masks[0][indexes, 1, :, :]
@@ -109,21 +109,21 @@ def main():
                 keep = nms(cls_dets)
                 all_boxes[j] = cls_dets[keep, :]
                 all_masks[j] = cls_masks[keep, :]
-            dets = [all_boxes[j] for j in range(1, num_classes)]
-            masks = [all_masks[j] for j in range(1, num_classes)]
+            dets = [all_boxes[j] for j in range(1, config.NUM_CLASSES)]
+            masks = [all_masks[j] for j in range(1, config.NUM_CLASSES)]
         else:
             masks = masks[0][:, 1:, :, :]
             im_height = np.round(im_shapes[0][0] / scales[0]).astype('int')
             im_width = np.round(im_shapes[0][1] / scales[0]).astype('int')
             print (im_height, im_width)
             boxes = clip_boxes(boxes[0], (im_height, im_width))
-            result_masks, result_dets = gpu_mask_voting(masks, boxes, scores[0], num_classes,
+            result_masks, result_dets = gpu_mask_voting(masks, boxes, scores[0], config.NUM_CLASSES,
                                                         100, im_width, im_height,
                                                         config.TEST.NMS, config.TEST.MASK_MERGE_THRESH,
                                                         config.BINARY_THRESH, 0)
 
-            dets = [result_dets[j] for j in range(1, num_classes)]
-            masks = [result_masks[j][:, 0, :, :] for j in range(1, num_classes)]
+            dets = [result_dets[j] for j in range(1, config.NUM_CLASSES)]
+            masks = [result_masks[j][:, 0, :, :] for j in range(1, config.NUM_CLASSES)]
         print 'testing {} {:.4f}s'.format(im_name, toc())
         # visualize
         for i in xrange(len(dets)):
@@ -132,9 +132,25 @@ def main():
             masks[i] = masks[i][keep]
         im = cv2.imread('../data/demo/' + im_name)
         im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
-        show_masks(im, dets, masks, classes)
+        show_masks(im, dets, masks, CLASSES)
 
     print 'done'
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Demonstrate a Mask R-CNN network')
+    parser.add_argument('--prefix', help='saved model prefix', default='model/e2e', type=str)
+    parser.add_argument('--epoch', help='epoch of pretrained model', default=0, type=int)
+    parser.add_argument('--gpu', help='GPU device to use', default=0, type=int)
+    args = parser.parse_args()
+    return args
+
+def main():
+    args = parse_args()
+    ctx = mx.gpu(args.gpu)
+    sym = get_resnet_test(num_classes=config.NUM_CLASSES, num_anchors=config.NUM_ANCHORS)
+    data, image_names = load_data()
+    predictor = get_net(data, sym, args.prefix, args.epoch, ctx)
+    demo_net(predictor, data, image_names)
 
 if __name__ == '__main__':
     main()
