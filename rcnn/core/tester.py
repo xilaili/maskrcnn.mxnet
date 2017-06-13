30448e7185084176006559ae6a9937d401c67d0e
xli47 <xli47@ncsu.edu>
2017-06-13 00:10:57 -0400

    updated core/tester.py

--- rcnn/core/tester.py
+++ rcnn/core/tester.py
@@ -35,6 +35,82 @@ class Predictor(object):
         self._mod.forward(data_batch)
         return [dict(zip(self._mod.output_names, _)) for _ in zip(*self._mod.get_outputs(merge_multi_context=False))]
 
+def im_proposal(predictor, data_batch, data_names, scale):
+    data_dict = dict(zip(data_names, data_batch.data))
+    output = predictor.predict(data_batch)
+
+    # drop the batch index
+    boxes = output['rois_output'].asnumpy()[:, 1:]
+    scores = output['rois_score'].asnumpy()
+
+    # transform to original scale
+    boxes = boxes / scale
+
+    return scores, boxes, data_dict
+
+
+def generate_proposals(predictor, test_data, imdb, vis=False, thresh=0.):
+    """
+    Generate detections results using RPN.
+    :param predictor: Predictor
+    :param test_data: data iterator, must be non-shuffled
+    :param imdb: image database
+    :param vis: controls visualization
+    :param thresh: thresh for valid detections
+    :return: list of detected boxes
+    """
+    assert vis or not test_data.shuffle
+    data_names = [k[0] for k in test_data.provide_data]
+
+    i = 0
+    t = time.time()
+    imdb_boxes = list()
+    original_boxes = list()
+    for im_info, data_batch in test_data:
+        t1 = time.time() - t
+        t = time.time()
+
+        scale = im_info[0, 2]
+        scores, boxes, data_dict = im_proposal(predictor, data_batch, data_names, scale)
+        t2 = time.time() - t
+        t = time.time()
+
+        # assemble proposals
+        dets = np.hstack((boxes, scores))
+        original_boxes.append(dets)
+
+        # filter proposals
+        keep = np.where(dets[:, 4:] > thresh)[0]
+        dets = dets[keep, :]
+        imdb_boxes.append(dets)
+
+        if vis:
+            vis_all_detection(data_dict['data'].asnumpy(), [dets], ['obj'], scale)
+
+        logger.info('generating %d/%d ' % (i + 1, imdb.num_images) +
+                    'proposal %d ' % (dets.shape[0]) +
+                    'data %.4fs net %.4fs' % (t1, t2))
+        i += 1
+
+    assert len(imdb_boxes) == imdb.num_images, 'calculations not complete'
+
+    # save results
+    rpn_folder = os.path.join(imdb.root_path, 'rpn_data')
+    if not os.path.exists(rpn_folder):
+        os.mkdir(rpn_folder)
+
+    rpn_file = os.path.join(rpn_folder, imdb.name + '_rpn.pkl')
+    with open(rpn_file, 'wb') as f:
+        cPickle.dump(imdb_boxes, f, cPickle.HIGHEST_PROTOCOL)
+
+    if thresh > 0:
+        full_rpn_file = os.path.join(rpn_folder, imdb.name + '_full_rpn.pkl')
+        with open(full_rpn_file, 'wb') as f:
+            cPickle.dump(original_boxes, f, cPickle.HIGHEST_PROTOCOL)
+
+    logger.info('wrote rpn proposals to %s' % rpn_file)
+    return imdb_boxes
+
 
 def im_detect(predictor, data_batch, data_names, scales):
     output_all = predictor.predict(data_batch)
@@ -207,4 +283,64 @@ def vis_all_mask(im_array, detections, masks, class_names, scale, cfg):
                            bbox=dict(facecolor=color, alpha=0.5), fontsize=12, color='white')
     plt.imshow(im)
     plt.show()
-    
+
+def vis_all_detection(im_array, detections, class_names, scale):
+    """
+    visualize all detections in one image
+    :param im_array: [b=1 c h w] in rgb
+    :param detections: [ numpy.ndarray([[x1 y1 x2 y2 score]]) for j in classes ]
+    :param class_names: list of names in imdb
+    :param scale: visualize the scaled image
+    :return:
+    """
+    import matplotlib.pyplot as plt
+    import random
+    im = image.transform_inverse(im_array, config.PIXEL_MEANS)
+    plt.imshow(im)
+    for j, name in enumerate(class_names):
+        if name == '__background__':
+            continue
+        color = (random.random(), random.random(), random.random())  # generate a random color
+        dets = detections[j]
+        for det in dets:
+            bbox = det[:4] * scale
+            score = det[-1]
+            rect = plt.Rectangle((bbox[0], bbox[1]),
+                                 bbox[2] - bbox[0],
+                                 bbox[3] - bbox[1], fill=False,
+                                 edgecolor=color, linewidth=3.5)
+            plt.gca().add_patch(rect)
+            plt.gca().text(bbox[0], bbox[1] - 2,
+                           '{:s} {:.3f}'.format(name, score),
+                           bbox=dict(facecolor=color, alpha=0.5), fontsize=12, color='white')
+    plt.show()
+
+
+def draw_all_detection(im_array, detections, class_names, scale):
+    """
+    visualize all detections in one image
+    :param im_array: [b=1 c h w] in rgb
+    :param detections: [ numpy.ndarray([[x1 y1 x2 y2 score]]) for j in classes ]
+    :param class_names: list of names in imdb
+    :param scale: visualize the scaled image
+    :return:
+    """
+    import cv2
+    import random
+    color_white = (255, 255, 255)
+    im = image.transform_inverse(im_array, config.PIXEL_MEANS)
+    # change to bgr
+    im = cv2.cvtColor(im, cv2.cv.CV_RGB2BGR)
+    for j, name in enumerate(class_names):
+        if name == '__background__':
+            continue
+        color = (random.randint(0, 256), random.randint(0, 256), random.randint(0, 256))  # generate a random color
+        dets = detections[j]
+        for det in dets:
+            bbox = det[:4] * scale
+            score = det[-1]
+            bbox = map(int, bbox)
+            cv2.rectangle(im, (bbox[0], bbox[1]), (bbox[2], bbox[3]), color=color, thickness=2)
+            cv2.putText(im, '%s %.3f' % (class_names[j], score), (bbox[0], bbox[1] + 10),
+                        color=color_white, fontFace=cv2.FONT_HERSHEY_COMPLEX, fontScale=0.5)
+    return im   
