a88eae424286a479f2b917378daba191f75d35a6
xli47 <xli47@ncsu.edu>
2017-06-18 13:11:25 -0400

    updated init.sh

--- init.sh
+++ init.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+mkdir -p model
 
 cd rcnn/cython
 python setup.py build_ext --inplace
-cd ../dataset/pycocotools
+cd ../pycocotools
 python setup.py build_ext --inplace
 cd ../mask
 python setup_linux.py build_ext --inplace
